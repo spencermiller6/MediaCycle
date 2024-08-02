bf8e1e885593205479a51c4db7cd2e09d429fca5
Spencer <spencermiller6@protonmail.com>
2024-08-01 21:33:09 -0700

    Added ReleaseTime.cs to tracking

---
+++ ReleaseTime.cs
@@ -0,0 +1,74 @@
+namespace ReleaseTime
+{
+    public class ReleaseTime
+    {
+        [Flags]
+        public enum DaysOfWeek
+        {
+            None = 0,
+            Sunday = 1 << 0,
+            Monday = 1 << 1,
+            Tuesday = 1 << 2,
+            Wednesday = 1 << 3,
+            Thursday = 1 << 4,
+            Friday = 1 << 5,
+            Saturday = 1 << 6,
+            Weekdays = Monday | Tuesday | Wednesday | Thursday | Friday,
+            Weekend = Saturday | Sunday,
+            EveryDay = Weekdays | Weekend
+        }
+
+        public DaysOfWeek Days;
+        public int Hour;
+        public int Minute;
+
+        public ReleaseTime(DaysOfWeek days, int hour, int minute)
+        {
+            if (hour < 1 || hour > 24)
+            {
+                throw new ArgumentOutOfRangeException(nameof(hour), "Hour must be between 1 and 24.");
+            }
+
+            if (minute < 0 || minute > 59)
+            {
+                throw new ArgumentOutOfRangeException(nameof(minute), "Minute must be between 0 and 59.");
+            }
+
+            Days = days;
+            Hour = hour;
+            Minute = minute;
+        }
+
+        public DateTime OnDate(DateTime date)
+        {
+            return new DateTime(date.Year, date.Month, date.Day, Hour, Minute, 0, 0, date.Kind);
+        }
+
+        public static DateTime? NextReleaseTime()
+        {
+            if (Program.ReleaseTimes.Count == 0)
+            {
+                return null;
+            }
+
+            DateTime today = DateTime.Now;
+            DateTime nextReleaseTime = Program.ReleaseTimes[0].OnDate(today).AddDays(1);
+
+            for (int i = Program.ReleaseTimes.Count - 1; i >= 0; i--)
+            {
+                DateTime releaseTime = Program.ReleaseTimes[i].OnDate(today);
+
+                if (releaseTime < today)
+                {
+                    break;
+                }
+                else
+                {
+                    nextReleaseTime = releaseTime;
+                }
+            }
+
+            return nextReleaseTime;
+        }
+    }
+}
