75d5bdd3b4a32c289080bc64af1f65900ead274b
Spencer <spencermiller6@protonmail.com>
2024-08-15 23:45:16 -0700

    Added support for passing in a channel to a feed command; fixed null reference error

--- Cli/Commands/FeedCommand.cs
+++ Cli/Commands/FeedCommand.cs
@@ -10,7 +10,7 @@ public class FeedCommand : Command
     public override string Name => "feed";
     public override string HelpText => "Show the feed of all channels within the current scope";
     public override int MinArguments => 0;
-    public override int MaxArguments => 0;
+    public override int MaxArguments => 1;
     public override List<string> Arguments => _arguments;
     public override List<Option> Options => _options;
 
@@ -21,12 +21,49 @@ public class FeedCommand : Command
     {
     }
 
+    public override void SetArguments(List<string> arguments)
+    {
+        base.SetArguments(arguments);
+        _arguments = arguments;
+    }
+
     public override void Execute()
     {
-        List<SyndicationItem> feed = GetFeedsFromFolder(Cli.Pwd);
-        List<SyndicationItem> sortedFeed = feed.OrderByDescending(item => item.PublishDate).ToList();
+        List<SyndicationItem> feed;
+
+        if (Arguments.Any())
+        {
+            feed = new List<SyndicationItem>();
+            foreach (string argument in Arguments)
+            {
+                RssChannel channel = GetChannelFromString(argument);
+                feed.AddRange(channel.Feed().Items);
+            }
+        }
+        else
+        {
+            feed = GetFeedsFromFolder(Cli.Pwd);
+        }
+
+        DateTime? releaseTime = ReleaseTime.NextReleaseTime();
+        List<SyndicationItem> filteredFeed = feed.Where(i => i.PublishDate < releaseTime).ToList();
+        List<SyndicationItem> sortedFeed = filteredFeed.OrderByDescending(item => item.PublishDate).ToList();
         
         ShowFeed(sortedFeed);
+    }
+
+    static RssChannel GetChannelFromString(string channelName)
+    {
+        RssChannel? channel = Cli.Pwd.Channels.FirstOrDefault(c => c.Name == channelName);
+
+        if(channel is null)
+        {
+            throw new Exception($"{channelName}: No such directory");
+        }
+        else
+        {
+            return channel;
+        }
     }
         
     static List<SyndicationItem> GetFeedsFromFolder(RssFolder parentFolder)
@@ -47,20 +84,14 @@ public class FeedCommand : Command
 
     static void ShowFeed(List<SyndicationItem> feed)
     {
-        DateTime? releaseTime = ReleaseTime.NextReleaseTime();
         int index = 0;
 
         foreach (SyndicationItem item in feed)
         {
-            if (item.PublishDate > releaseTime)
-            {
-                break;
-            }
-
             Console.WriteLine(index++);
             Console.WriteLine($"Title: {item.Title.Text}");
             Console.WriteLine($"Published Date: {item.PublishDate}");
-            Console.WriteLine($"Summary: {item.Summary.Text}");
+            Console.WriteLine($"Summary: {item.Summary?.Text ?? string.Empty}");
             Console.WriteLine($"Link: {item.Id}");
             Console.WriteLine($"Author: {RssChannel.GetAuthors(item)}");
             Console.WriteLine();
