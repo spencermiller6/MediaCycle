f59e0ba60e629bab6634882759d27e26c2d26629
Spencer <spencermiller6@protonmail.com>
2024-09-02 19:16:24 -0700

    Removed references to deleted commands

--- Program.cs
+++ Program.cs
@@ -1,19 +1,19 @@
 ﻿using System.CommandLine;
-using MediaCycle.Cli;
 using MediaCycle.Cli.Commands;
 
 public class Program
 {
     static async Task<int> Main(string[] args)
     {
-        var rootCommand = new RootCommand("Sample app for System.CommandLine");
+        var rootCommand = new RootCommand
+        {
+            Name = "mediacycle",
+            Description = "A CLI RSS reader designed to control the release of content"
+        };
 
-        rootCommand.AddCommand(CdCommand.Create());
         rootCommand.AddCommand(ConnectCommand.Create());
         rootCommand.AddCommand(FeedCommand.Create());
-        rootCommand.AddCommand(LsCommand.Create());
         rootCommand.AddCommand(OpenCommand.Create());
-        rootCommand.AddCommand(PwdCommand.Create());
         rootCommand.AddCommand(SyncCommand.Create());
 
         return await rootCommand.InvokeAsync(args);
